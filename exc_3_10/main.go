bba4100d58b538aff4d5f4ddd7b167706190b5b9
qihaoyu <qihaoyu@bytedance.com>
2021-01-28 13:40:00 +0800

    exc_3_10

---
+++ exc_3_10/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bytes"
+	"os"
+	"fmt"
+)
+
+func main(){
+	if len(os.Args) <= 1{
+		return
+	}
+
+	var s string = os.Args[1]
+	fmt.Printf("The ans from comma is: %s\n",comma(s))
+}
+
+
+
+// comma inserts commas in a non-negative decimal integer string.
+func comma(s string) string {
+	var buf bytes.Buffer
+
+	start := len(s)%3
+	buf.WriteString(s[:start])
+
+	for ;start+3<=len(s);start+=3{
+		buf.WriteByte(',')
+		buf.WriteString(s[start:start+3])
+	}
+
+	return buf.String()
+}
