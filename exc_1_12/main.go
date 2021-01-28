f09b7c560490314f7848fcf58e65b554fd4a165f
qihaoyu <qihaoyu@bytedance.com>
2021-01-28 13:12:37 +0800

    exc_1_12

---
+++ exc_1_12/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"image"
+	"image/gif"
+	"log"
+	"math"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"sync"
+	"image/color"
+	"io"
+	"time"
+)
+
+var palette = []color.Color{color.White, color.Black}
+
+const (
+	whiteIndex = 0 // first color in palette
+	blackIndex = 1 // next color in palette
+)
+
+const (
+	key = "cycles"
+)
+
+var mu sync.Mutex
+var count int
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// update the rand seed everytime handler is called
+
+	// The sequence of images is deterministic unless we seed
+	// the pseudo-random number generator using the current time.
+	// Thanks to Randall McPherson for pointing out the omission.
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	// parse url to get query
+	// FormValue find key value pairs in query
+	// Post and Put body have precedence over query string
+	valuestring := r.FormValue(key)
+	value, err := strconv.Atoi(valuestring)
+	if err != nil{
+		// set to default value
+		value = 5
+	}
+
+	lissajous(w, value)
+}
+
+
+func lissajous(out io.Writer, value int) {
+	const (
+		res     = 0.001 // angular resolution
+		size    = 100   // image canvas covers [-size..+size]
+		nframes = 64    // number of animation frames
+		delay   = 8     // delay between frames in 10ms units
+	)
+
+	cycles := value
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < float64(cycles)*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
+				blackIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
+}
